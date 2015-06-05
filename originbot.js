b39a84d86b1686be1ddf242f6caf240ab63b985d
raurir <raurir@gmail.com>
2015-06-05 14:11:17 +1000

    1 minute +

--- originbot.js
+++ originbot.js
@@ -266,7 +266,7 @@ function initBot() {
     }
 
     function doItAgain() {
-      var delayMins = Math.round((2 + Math.random() * 3) * 100) / 100;
+      var delayMins = Math.round((1 + Math.random() * 3) * 100) / 100;
       doInSpecificMinutes(delayMins);
     }
 
