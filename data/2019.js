87d4b968a84b0a1c9697d18ebbc88a39649fc8c7
Rauri Rochford <raurir@gmail.com>
2019-06-06 00:17:20 +1000

    almost converted into player data

---
+++ data/2019.js
@@ -0,0 +1,2 @@
+crowd: 52,191
+D.Gagai 2, tries; Ponga)
--- pages/generate.js
+++ pages/generate.js
@@ -5,6 +5,22 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const mkdir = util.promisify(fs.mkdir);
 
+if (!Array.prototype.flat) {
+  Array.prototype.flat = function(depth) {
+    var flattend = [];
+    (function flat(array, depth) {
+      for (let el of array) {
+        if (Array.isArray(el) && depth > 0) {
+          flat(el, depth - 1);
+        } else {
+          flattend.push(el);
+        }
+      }
+    })(this, Math.floor(depth) || 1);
+    return flattend;
+  };
+}
+
 const local = true;
 
 const header = ({ title }) => `<html>
@@ -38,10 +54,10 @@ const footer = ({ local }) => `${
 
 const writeFileInDir = async (path, file, data) => {
   const fullPath = path + file;
-  console.log("writing:", fullPath);
   let createDir = false;
   try {
     await writeFile(fullPath, data);
+    // console.log("writing:", fullPath);
     return true;
   } catch (err) {
     if (err.code === "ENOENT") {
@@ -66,6 +82,78 @@ const writeFileInDir = async (path, file, data) => {
   }
 };
 
+/*
+generate error TypeError: Z.Niszczot 2 is not a function
+    at Array.map (<anonymous>)
+*/
+
+const extractPoints = (t, type, attr) => {
+  if (new RegExp(attr).test(type)) {
+    if (t[attr]) throw new Error("duplicate attr: " + attr);
+    t[attr] = type
+      .replace(` ${attr}`, "")
+      .split(",")
+      .map(person => {
+        let lastBit = person.split(" ");
+        lastBit = lastBit[lastBit.length - 1];
+        const n = Number(lastBit);
+        if (n === Number(lastBit)) {
+          const index = person.lastIndexOf(lastBit) - 1;
+          const p = person.substr(0, index);
+          person = Array(n).fill(p);
+        }
+        return person;
+      })
+      .flat();
+  }
+};
+
+const extract = detail => {
+  const obj = {};
+  let t;
+  if (/Queensland/.test(detail)) {
+    obj.qld = {
+      total: detail.split(" ")[1]
+    };
+    t = obj.qld;
+  }
+  if (/New South Wales/.test(detail)) {
+    obj.nsw = {
+      total: detail.split(" ")[3]
+    };
+    if (t) throw new Error("duplicate state");
+    t = obj.nsw;
+  }
+  if (!t) return; ///throw new Error("no year");
+  t.total = Number(t && t.total);
+
+  const d = detail.match(/\((.*)\)/);
+  if (!(d && d[1])) {
+    return console.log("no game details");
+  }
+  d[1].split(";").map(type => {
+    extractPoints(t, type, "tries");
+    extractPoints(t, type, "goals");
+    extractPoints(t, type, "conversions");
+    extractPoints(t, type, "dropkicks");
+  });
+  return obj;
+};
+
+const parse = data =>
+  data
+    //.splice(0, 3) //
+    .map(year => {
+      year.matches.map(match => {
+        const { details } = match;
+        let m = {};
+        details.split("\n").forEach(detail => {
+          m = Object.assign({}, m, extract(detail));
+        });
+        console.log(year.year, m);
+      });
+    });
+
 const generate = async () => {
   try {
     const buffer = await readFile("../js/data.js");
@@ -74,9 +162,16 @@ const generate = async () => {
     const stringObject = string.substr(12);
     // cannot JSON.parse because it's malformed JSON.
     const data = eval(stringObject);
+
+    parse(data);
+    return;
     const yearRoot = await generateYearRoot(data);
     if (!yearRoot) console.log("generate error creating yearRoot");
-    const years = await Promise.all(data.map(generateYear));
+    const years = await Promise.all(
+      data
+        // .slice(0, 3) // ok
+        .map(generateYear)
+    );
     if (!years.every(Boolean))
       console.log("generate error creating years", years);
     console.log("generate complete");
@@ -112,6 +207,10 @@ const generateYear = async data => {
   try {
     // console.log(JSON.stringify(data, null, " "));
     const { NSW, QLD, winner, matches, year } = data;
+
+    console.log("========");
+    console.log(year);
+
     const html = `${header({ title: `Year ${year}` })}
     <div>
       <h1>Stats Of Origin</h1>
@@ -146,7 +245,7 @@ const generateYear = async data => {
     </div>
     ${footer({ local })}`;
 
-    await writeFileInDir(`../deploy/years/${year}/`, "index.html", html);
+    // await writeFileInDir(`../deploy/years/${year}/`, "index.html", html);
     return true;
   } catch (err) {
     console.log("generateYear error", err);
